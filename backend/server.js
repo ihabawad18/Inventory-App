6de6c43c0a33626f4b76ac22b7b02393fe7635c5
DESKTOP-1S5K0HB\CLICK ONCE <ihabawad757@gmail.com>
2023-11-19 17:51:59 +0200

    commiting recent changes

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const errorHandler = require("./middlewares/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require( 'path');
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARES
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+
+// ROUTES MIDDLEWARES
+
+app.use("/api/users",userRoute);
+
+app.use("/api/products",productRoute);
+
+// Error Middleware
+app.use(errorHandler);
 
 // Connect to DB and start server
 
-mongoose.connect(process.env.MONGO_URI).then(
+const PORT = process.env.PORT || 5000;
+mongoose.connect("mongodb://localhost:27017").then(
     ()=>{
         app.listen(PORT,()=>{
             console.log(`Server Running on port ${PORT}`);
